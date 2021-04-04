dc521fba0b71b743b4ba9e6b86bfa9707d711ce7
Christopher Yin <ctyin@seas.upenn.edu>
2021-04-04 20:59:55 +0000

    get_inps

--- trace_txes.py
+++ trace_txes.py
@@ -39,11 +39,18 @@ class TXO:
     def from_tx_hash(cls,tx_hash,n=0):
         tx_dict = rpc_connection.getrawtransaction(tx_hash, True)
         
-        print(tx_dict['vout'][n])
-        
         TXO(tx_dict['hash'], tx_dict['vout'][n]['n'], tx_dict['vout'][n]['value'], tx_dict['vout'][n]['scriptPubKey']['addresses'][0], tx_dict['time'])
 
     def get_inputs(self,d=1):
-        pass
-        #YOUR CODE HERE
+        tx_dict = rpc_connection.getrawtransaction(self.tx_hash, True)
+        self.inputs = tx_dict['vin']
+        last_round = tx_dict['vin']
+        
+        for _ in range(d - 1):
+            curr_round = []
+            for inp in last_round:
+                vins = rpc_connection.getrawtransaction(self.tx_hash, True)['vin']
+                curr_round.extend(vins)
+            self.inputs.extend(curr_round)
+            last_round = curr_round
 
